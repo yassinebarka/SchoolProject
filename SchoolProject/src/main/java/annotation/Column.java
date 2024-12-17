eab7501dda1ce9ebd7119ad4bdc11a368a484e62
ABDELHAKIM SATOUT <hakim.satout@gmail.com>
2024-12-17 19:10:10 +0100

    Add annotations

---
+++ SchoolProject/src/main/java/annotation/Column.java
@@ -0,0 +1,17 @@
+package com.hyh.schoolmanagement.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Column {
+    String name();
+    String type();
+    boolean primaryKey() default false;
+
+    String foreignKey() default "";
+}
+
